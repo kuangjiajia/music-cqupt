845878693d165d94ab7160711f81f1cd10e8ff24
kuangjiajia <1298201366@qq.com>
2018-04-20 19:39:21 +0800

    change router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Root from './router/index'
 import { store } from './store/index.js'
 
 const App = () => (
     <Provider store={store}>
-        <BrowserRouter basename="/nodejs/music-cqupt/build">
+        <HashRouter>
             <Root />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 )
 
